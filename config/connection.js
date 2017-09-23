44f00555ee9879ca91b160c6cd4fd56d7c04290f
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-09-23 13:05:51 -0400

    small fix

--- config/connection.js
+++ config/connection.js
@@ -8,3 +8,15 @@ var connection = mysql.createConnection({
   password: "12345",
   database: "burgers_db"
 });
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
