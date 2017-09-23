9572f1b7ab55a2ed9a3deab84341ac64e728ecc6
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-09-23 09:19:37 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
-'use strict';
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
-var express = require('express');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
-var Sequelize = require('sequelize');
-var models = require('./models');
+var port = process.env.PORT || 3000;
+
 var app = express();
 
-app.use(express.static(process.cwd() + '/public'));
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
-app.use(methodOverride('_method'))
-//EXPORT TO HANDLEBAARS THE TEMPLATE WITH A MUSTACHE!
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-	defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var routes = require('./controllers/burgers_controller.js');
-app.use('/', routes);
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 
-const port = process.env.PORT || 3000;
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-app.listen(port, function(){
-	console.log(`listening on port ${port}`);
-});
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
