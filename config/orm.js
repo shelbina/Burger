ea2ce0a322a9551a7d56f6f6fe4922646588ce76
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-09-23 13:15:08 -0400

    major clean up

--- config/orm.js
+++ config/orm.js
@@ -1,92 +1,63 @@
-// Import MySQL connection.
-var connection = require("../config/connection.js");
+var connection = require('../config/connection.js');
 
-// Helper function for SQL syntax.
-function printQuestionMarks(num) {
+function printQuestionMarks(num){
   var arr = [];
 
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
+  for (var i=0; i<num; i++){
+    arr.push('?')
+  };
 
   return arr.toString();
-}
+};
 
-// Helper function for SQL syntax.
-function objToSql(ob) {
+function objToSql(ob){
+  //column1=value, column2=value2,...
   var arr = [];
 
   for (var key in ob) {
-    if (Object.hasOwnProperty.call(ob, key)) {
-      arr.push(key + "=" + ob[key]);
-    }
-  }
+    arr.push(key + '=' + ob[key]);
+  };
 
   return arr.toString();
-}
-
-// Object for all our SQL statement functions.
-var orm = {
-  all: function(tableInput, cb) {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-  create: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
-
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
-
-    console.log(queryString);
-
-    connection.query(queryString, vals, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-  // An example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
-
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
-
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
-  delete: function(table, condition, cb) {
-    var queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += condition;
-
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  }
 };
 
-// Export the orm object for the model (cat.js).
-module.exports = orm;
+var orm = {
+	all: function(tableInput, cb){
+		var queryString = 'SELECT * FROM ' + tableInput;
+
+		connection.query(queryString, function(err, result){
+			if(err) throw err;
+			cb(result);
+		});
+	},
+	create: function(table, col, vals, cb){
+		var queryString = 'INSERT INTO ' + table;
+		queryString = queryString + ' (';
+		queryString = queryString + col.toString(); 
+		queryString = queryString + ') ';
+		queryString = queryString + 'VALUES (';
+		queryString = queryString + printQuestionMarks(vals.length);
+		queryString = queryString + ') ';
+
+		connection.query(queryString, vals, function(err, result){
+			if(err) throw err;
+			cb(result);
+		});
+	},
+	update: function(table, objColVals, condition, cb){
+		var queryString = 'UPDATE ' + table;
+		queryString = queryString + ' SET ';
+		queryString = queryString + objToSql(objColVals);
+		queryString = queryString + ' WHERE ';
+		queryString = queryString + condition;
+
+		console.log(queryString);
+
+		connection.query(queryString, function(err, result){
+			if(err) throw err;
+			cb(result);
+		});
+	}
+};
+
+module.exports=orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,50 +1,35 @@
-var express = require("express");
-
+var express = require('express');
 var router = express.Router();
+var burgers = require('../models/burger.js');
 
-// Import the model (cat.js) to use its database functions.
-var burger = require("../models/burgers.js");
-
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+router.get('/', function(req, res){
+	res.redirect('/burgers')
 });
 
-router.post("/burgers", function(req, res) {
-  burger.create([
-    "name",
-  ], [
-    req.body.name,
-  ], function() {
-    res.redirect("/");
-  });
+router.get('/burgers', function(req, res){
+	burgers.all(function(data){
+		var hbsObject = {burgers: data};
+
+		console.log(hbsObject);
+
+		res.render('index', hbsObject);
+	});
 });
 
-router.put("/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.update({
-    devoured: true
-  }, condition, function(data) {
-    res.redirect("/");
-  });
+router.post('/burgers/create', function(req, res){
+	burgers.create(['burger_name'], [req.body.b_name], function(data){
+		res.redirect('/burgers')
+	});
 });
 
-router.delete("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+router.put('/burgers/update/:id', function(req, res){
+	var condition = 'id = ' + req.params.id;
 
-  burger.delete(condition, function() {
-    res.redirect("/");
-  });
+	console.log('condition ', condition);
+
+	burgers.update({'devoured': req.body.devoured}, condition, function(data){
+		res.redirect('/burgers');
+	});
 });
 
-// Export routes for server.js to use.
 module.exports = router;
