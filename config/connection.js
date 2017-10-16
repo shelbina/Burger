6c5760e5cf649d4f76b858f2a0cdf5a74b1c37d6
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-10-16 19:47:47 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,6 @@
 // Node Dependency
 var mysql = require('mysql');
 var connection;
-var JAWSDB_URL = mysql:swg5sv3v5c9ku65q:x4m1xnox6onpsu44@cdm1s48crk8itlnr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mizc089jp0b7ut6s
 
 // For Heroku Deployment vs. Local MySQL Database
 if(process.env.JAWSDB_URL){
@@ -16,13 +15,10 @@ else{
     });
 };
 
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
+connection.connect(function(err){
+    if(err) throw err;
+    console.log('Connect to database with id:',connection.threadId)
 });
 
-// Export the Connection
 module.exports = connection;
+
--- database/schema_JawsDB.sql
+++
@@ -1,8 +0,0 @@
-
-Use swg5sv3v5c9ku65q;
-
-CREATE TABLE burgers (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	burger_name varchar(50),
-	devoured boolean,
-	date timestamp);
